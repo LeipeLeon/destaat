da7466f8586b00c0f02eb9c99b4ce16e28acc8a4
Leon Berenschot <leipeleon@gmail.com>
2017-11-10 18:41:56 +0100

    Value of simultanious connections on the smallest instance on DO

--- loadtest.sh
+++ loadtest.sh
@@ -2,7 +2,7 @@
 # ffmpeg -i http://192.168.3.23/live -r 15 http://188.226.129.169:8090/destaat.ffm
 #
 i=1
-while [ $i -le 50 ]
+while [ $i -le 300 ]
 do
   echo "Process \"$i\" started";
   curl http://188.226.129.169:8090/destaat.mjpg -s > /dev/null 2>&1 & pid=$!
