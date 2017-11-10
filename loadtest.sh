71a8d968639f0f8fcaaf12602ba11ca1f9ba9043
Leon Berenschot <leipeleon@gmail.com>
2017-11-10 13:19:08 +0100

    Add a load tester

---
+++ loadtest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ffmpeg -i http://192.168.3.23/live -r 15 http://188.226.129.169:8090/destaat.ffm
+#
+i=1
+while [ $i -le 50 ]
+do
+  echo "Process \"$i\" started";
+  curl http://188.226.129.169:8090/destaat.mjpg -s > /dev/null 2>&1 & pid=$!
+  # sleep 1 & pid=$!
+  PID_LIST+=" $pid";
+  i=$(( $i + 1 ))
+done
+
+trap "kill ${PID_LIST}" SIGINT
+
+echo "Parallel processes have started";
+
+wait $PID_LIST
+
+echo
+echo "All processes have completed";
